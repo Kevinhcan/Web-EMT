f58b161a65408760af0652d11d23f8df20654633
Kevinhcan <kgarcesmartinez@gmail.com>
2024-12-26 23:57:16 -0500

    socialMedial add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Portada from "./components/Portada";
 
 function App() {
   return (
     <>
       <header className="Encabezado">
         <div className="Indice">
-          <Navbar>
-            
-          </Navbar>
+          <Portada></Portada>
+          <Navbar></Navbar>
+
         </div>
       </header>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,17 +13,29 @@ function Navbar() {
   return (
     <>
       <NavContainer>
-        <h2>
-          <img src="public/cover.png" alt="" />
-        </h2>
+        <h2></h2>
         <div className={`links ${clicked ? "active" : ""}`}>
-        <a onClick = {handleClick} href="#h">Inicio</a>
-          <a onClick = {handleClick} href="#h">Calibraciones acréditadas</a>
-          <a onClick = {handleClick} href="#h">Termometros</a>
-          <a onClick = {handleClick} href="#h">Alcoholimetros</a>
-          <a onClick = {handleClick} href="#h">Detectores de gas</a>
-          <a onClick = {handleClick} href="#h">Medidores ambientales</a>
-          <a onClick = {handleClick} href="#h">Contactanos</a>
+          <a onClick={handleClick} href="#h">
+            Inicio
+          </a>
+          <a onClick={handleClick} href="#h">
+            Calibraciones acréditadas
+          </a>
+          <a onClick={handleClick} href="#h">
+            Termometros
+          </a>
+          <a onClick={handleClick} href="#h">
+            Alcoholimetros
+          </a>
+          <a onClick={handleClick} href="#h">
+            Detectores de gas
+          </a>
+          <a onClick={handleClick} href="#h">
+            Medidores ambientales
+          </a>
+          <a onClick={handleClick} href="#h">
+            Contactanos
+          </a>
           {/* Cambiar a 
           -Nuestros productos
           -Nuestros servicios
@@ -44,20 +56,14 @@ function Navbar() {
 export default Navbar;
 
 const NavContainer = styled.nav`
-  img {
-    color: #e8e8e8;
-    margin-left:0.3rem;
-    margin-top:0.5rem;
-    border-radius:10px;
-    background-color:#cdcdcd7d;
-    height: 90px; /* Cambia el ancho */
-    
-  
-  }
-  
+  height:4.5rem;
   background: rgb(28, 31, 76);
-  background: rgb(212,19,90);
-background: linear-gradient(90deg, rgba(212,19,90,1) 0%, rgba(28,31,76,1) 69%);
+  background: rgb(212, 19, 90);
+  background: linear-gradient(
+    90deg,
+    rgba(212, 19, 90, 1) 0%,
+    rgba(28, 31, 76, 1) 69%
+  );
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -77,12 +83,13 @@ background: linear-gradient(90deg, rgba(212,19,90,1) 0%, rgba(28,31,76,1) 69%);
 
     a {
       color: #e5e5e5/0;
-      font-size: 0rem;
       display: block;
+      transition:1000ms;
       &:hover {
         background-color: #ffffff11;
-        transition: 200ms;
-        padding: 0.2rem;
+        transition: 500ms;
+        padding-left:10px;
+        padding-right:10px;
         border-radius: 7px;
       }
     }
@@ -138,8 +145,12 @@ const BgDiv = styled.div`
   z-index: -1;
   &.active {
     border-radius: 0 0 80% 0;
-    background: rgb(212,19,90);
-background: linear-gradient(90deg, rgba(212,19,90,1) 0%, rgba(28,31,76,1) 69%);
+    background: rgb(212, 19, 90);
+    background: linear-gradient(
+      90deg,
+      rgba(212, 19, 90, 1) 0%,
+      rgba(28, 31, 76, 1) 69%
+    );
     transition: 300ms;
     width: 100%;
     height: 100%;
